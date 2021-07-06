ae5507ecfe7840df68210c998842007b26653d81
Joel Corrales <jcorralesg@est.ups.edu.ec>
2021-07-06 09:37:43 -0500

    Realizando Cambios

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-    cout << "INTENTO PARA VER SI FUNCIONA EL REPOSITORIO REMOTO DE GITHUB" << endl;
+    cout << "INTENTO PARA VER SI FUNCIONA EL REPOSITORIO REMOTO DE GITHUBxxxx" << endl;
     return 0;
 }
